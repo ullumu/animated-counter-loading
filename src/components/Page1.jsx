87c9edc4840afb3def101090a33cc565fc648775
ullumu <ullumu18@gmail.com>
2026-06-27 11:42:15 +0700

    buat part loading

---
+++ src/components/Page1.jsx
@@ -0,0 +1,13 @@
+const background = "bg-blue";
+
+function Page1(props) {
+    return (
+        <div className={`relative min-h-screen flex bg-black`}>
+            <div className="container max-w-screen-xl mx-auto flex justify-center items-center text-4xl text-white">
+                Page 1
+            </div>
+        </div>
+    );
+}
+
+export default Page1;
